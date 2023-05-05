6082d7bf4d6aeb5f627b64b85e49175fb48cfe41
Zisan-art <79694731+Zisan-art@users.noreply.github.com>
2023-05-05 12:16:01 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
-customhost = "employee.cspnc5ouklrd.us-east-1.rds.amazonaws.com"
+customhost = "employee.cadbupwobwlq.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee"
+custombucket = "chuazisan-employee"
 customregion = "us-east-1"
-
