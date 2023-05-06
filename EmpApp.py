589565998193aab6df3c4690731f19eb8fb68a97
Zisan-art <79694731+Zisan-art@users.noreply.github.com>
2023-05-06 17:04:23 +0800

    Add files via upload

--- EmpApp.py
+++ EmpApp.py
@@ -38,45 +38,20 @@ def AddEmp():
     last_name = request.form['last_name']
     pri_skill = request.form['pri_skill']
     location = request.form['location']
-    salary = request.form['salary']
-    othours = request.form['othours']
-    emp_image_file = request.files['emp_image_file']
-
-    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s, %s, %s)"
-    select_sql = "SELECT * FROM employee WHERE emp_id = (%s)"
-    cursor = db_conn.cursor()
-    cursor.execute(select_sql,(emp_id))
-    if emp_image_file.filename == "":
-        return "Please select a file"
-    if cursor.fetchone() is not None:
-        return "Employee ID already exist"
-    try:   
-        cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location, salary, othours))
-
-   
-@app.route("/addemp", methods=['POST'])
-def AddEmp():
-    return render_template('AddEmp.html')
-    emp_id = request.form['emp_id']
-    first_name = request.form['first_name']
-    last_name = request.form['last_name']
-    pri_skill = request.form['pri_skill']
-    location = request.form['location']
     job_title = request.form['job_title']
     salary = request.form['salary']
     bonus = request.form['bonus']
     emp_image_file = request.files['emp_image_file']
 
     insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s, %s, %d, %d)"
-    select_sql = "SELECT * FROM employee WHERE emp_id = (%s)"
     cursor = db_conn.cursor()
-    cursor.execute(select_sql,(emp_id))
+
     if emp_image_file.filename == "":
         return "Please select a file"
-    if cursor.fetchone() is not None:
-        return "Employee ID already exist"
-    try:   
-        cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location, job_title, salary, bonus))
+
+    try:
+
+        cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location))
         db_conn.commit()
         emp_name = "" + first_name + " " + last_name
         # Uplaod image file in S3 #
@@ -98,8 +73,6 @@ def AddEmp():
                 s3_location,
                 custombucket,
                 emp_image_file_name_in_s3)
-            
-            print(object_url)
 
         except Exception as e:
             return str(e)
@@ -109,7 +82,6 @@ def AddEmp():
 
     print("all modification done...")
     return render_template('AddEmpOutput.html', name=emp_name)
-
 
 @app.route("/getemp", methods=['GET', 'POST'])
 def getpage():
