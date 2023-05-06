03e6ef521738bfb23203174d3ac95e7e3ea3cf08
Zisan-art <79694731+Zisan-art@users.noreply.github.com>
2023-05-06 15:00:17 +0800

    Delete tables.py

--- tables.py
+++
@@ -1,8 +0,0 @@
-from flask_table import Table, Col, LinkCol
- 
-class Results(Table):
-    emp_id = Col('emp_id')
-    job_title = Col('job_title')
-    salary = Col('salary')
-    edit = LinkCol('Edit', 'edit_view', url_kwargs=dict(id='emp_id'))
-    delete = LinkCol('Delete', 'delete_user', url_kwargs=dict(id='emp_id'))
